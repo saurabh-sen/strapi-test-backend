97ccaec5a84e8cfb8f451a2d9e7927933286286f
Saurabh Sen <sensaurabh1212@gmail.com>
2023-03-26 15:24:33 +0530

    added something

--- config/database.js
+++ config/database.js
@@ -71,21 +71,36 @@
 // };
 
 
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
+    },
+    useNullAsDefault: true,
+  },
+});
+
     // strapi-api/config/database.js
-    module.exports = ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST', 'localhost'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'strapi-tut'),
-          user: env('DATABASE_USERNAME', 'postgres'),
-          password: env('DATABASE_PASSWORD', 'postgres@saurabh11'),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
-          // ssl: {
-          //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-          // },
-        },
-        debug: false,
-      },
-    });
+    // module.exports = ({ env }) => ({
+    //   connection: {
+    //     client: 'postgres',
+    //     connection: {
+    //       host: env('DATABASE_HOST', 'localhost'),
+    //       port: env.int('DATABASE_PORT', 5432),
+    //       database: env('DATABASE_NAME', 'strapi-tut'),
+    //       user: env('DATABASE_USERNAME', 'postgres'),
+    //       password: env('DATABASE_PASSWORD', 'postgres@saurabh11'),
+    //       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+    //       // ssl: {
+    //       //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+    //       // },
+    //     },
+    //     debug: false,
+    //   },
+    // });
